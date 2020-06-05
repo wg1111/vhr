52c39b612a1046ffb6cbbc35928f5d387c589d19
wei.gang <2012520497@qq.com>
2020-06-05 10:13:20 +0800

    异常处理

--- src/main/java/com/wg/vhr/exception/GlobalExceptionHandler.java
+++ src/main/java/com/wg/vhr/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.wg.vhr.exception;
 
+import com.mysql.jdbc.exceptions.MySQLIntegrityConstraintViolationException;
 import com.wg.vhr.model.ResponseBean;
 import org.apache.ibatis.jdbc.SQL;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -18,6 +19,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(SQLException.class)
     public ResponseBean SQLException(SQLException e){
+        if (e instanceof MySQLIntegrityConstraintViolationException) {
+            return ResponseBean.error("数据库中有关联数据，删除失败！");
+        }
         return ResponseBean.error("数据库异常，操作失败！");
     }
 }
